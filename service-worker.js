9c9df69df34b4e6b6d4fdd004d12e48d6aa5170b
smartidea <71277014+herutama7782@users.noreply.github.com>
2025-10-03 07:54:41 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,54 +1,34 @@
-const CACHE_NAME = 'POS Mobile PWA';
-const urlsToCache = [
-  "/index.css",
-  "/index.js"
-];
+const cacheName = "Omsetin UMKM"
+const preCache = ["/", "/style.css", "/script.js"]
 
-// Install event
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+self.addEventListener("install", (e) => {
+  console.log("service worker installed")
 
-// Fetch event - cache-first strategy
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, responseToCache);
-          });
-          return response;
-        });
-      })
-  );
-});
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName)
+      cache.addAll(preCache)
+    })(),
+  )
+})
 
-// Activate event
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const cache = await caches.open(cacheName)
+      const resCache = await cache.match(e.request)
+
+      if (resCache) return resCache
+
+      try {
+        const res = await fetch(e.request)
+
+        cache.put(e.request, res.clone())
+        return res
+      } catch (error) {
+        console.log(error)
+      }
+    })(),
+  )
+})
+
